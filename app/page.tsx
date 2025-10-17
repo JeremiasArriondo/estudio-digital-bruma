d3f15b365f4e8114feb810390276ef53fd251520
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2025-10-16 23:04:46 -0300

    fix: footer copy

--- app/page.tsx
+++ app/page.tsx
@@ -378,8 +378,7 @@ export default function Home() {
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-4 md:mb-0">
               <p className="text-sm text-violet-primary/70 dark:text-purple-400/70">
-                © {new Date().getFullYear()} WebDesign. Todos los derechos
-                reservados.
+                © {new Date().getFullYear()} BrumaDesign
               </p>
             </div>
             <div className="flex space-x-4">
