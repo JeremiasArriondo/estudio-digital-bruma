388a5f37c69812907866b43b70932a17c595f721
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2025-10-21 01:04:54 -0300

    feat: aria label add

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,7 @@ export default function Home() {
       <WhatsAppButton
         className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg hover:scale-110"
         icon={<MessageCircle size={24} />}
+        aria-label="Contactar por WhatsApp"
       />
 
       {/* Navbar */}
