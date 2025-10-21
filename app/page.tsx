2614f84ae106fe8a5832e2307c768a427bd1ffee
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2025-10-21 01:05:26 -0300

    fix: contrast colors

--- app/page.tsx
+++ app/page.tsx
@@ -323,7 +323,7 @@ export default function Home() {
         <div className="container px-4 md:px-6">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-4 md:mb-0">
-              <p className="text-sm text-violet-primary/70 dark:text-purple-400/70">
+              <p className="text-sm text-violet-primary dark:text-purple-400">
                 © {new Date().getFullYear()} BrumaDesign
               </p>
             </div>
